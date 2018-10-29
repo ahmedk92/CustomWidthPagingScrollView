a59515a1482f81754d7330462f7697c61a21dcb5
Ahmed Khalaf <ahmedkhalaf.92@gmail.com>
2018-10-29 20:24:34 +0200

    Initial Commit

---
+++ CustomWidthPagingScrollView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomWidthPagingScrollView
+//
+//  Created by Ahmed Khalaf on 10/29/18.
+//  Copyright © 2018 Ahmed Khalaf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
